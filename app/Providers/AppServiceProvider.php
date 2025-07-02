6206037d5d346e0cd6611e82581d6e544b1534ac
Tim Anthony Alexander <info@timanthonyalexander.de>
2025-07-02 11:00:40 +0200

    Add HTTPS

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,21 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Force HTTPS URLs in development environment since we're using SSL proxy
+        if ($this->app->environment('local')) {
+            \Illuminate\Support\Facades\URL::forceScheme('https');
+            \Illuminate\Support\Facades\URL::forceRootUrl('https://shmallergies.test:2811');
+        }
+        
+        // Trust all proxies in development (for SSL proxy)
+        if ($this->app->environment('local')) {
+            $this->app['request']->setTrustedProxies(['127.0.0.1', '::1'], 
+                \Illuminate\Http\Request::HEADER_X_FORWARDED_FOR |
+                \Illuminate\Http\Request::HEADER_X_FORWARDED_HOST |
+                \Illuminate\Http\Request::HEADER_X_FORWARDED_PORT |
+                \Illuminate\Http\Request::HEADER_X_FORWARDED_PROTO |
+                \Illuminate\Http\Request::HEADER_X_FORWARDED_AWS_ELB
+            );
+        }
     }
 }
